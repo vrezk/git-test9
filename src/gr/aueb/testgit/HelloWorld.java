1305e441cce5f7dd60add12fed667c0f40793a6d
vrezk <vrez.kh@gmail.com>
2025-12-02 20:27:16 +0200

    xoxoxo

--- src/gr/aueb/testgit/HelloWorld.java
+++ src/gr/aueb/testgit/HelloWorld.java
@@ -2,7 +2,7 @@ package gr.aueb.testgit;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World χοχοχο!!");
     }
 }
 
